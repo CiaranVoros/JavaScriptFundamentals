7e8f8c8f8a791290377dffe9a117a4a0689b60fb
CiaranVoros <ciaranvoros@gmail.com>
2023-01-06 20:11:48 -0800

    setting ages

--- first-jsx/src/App.js
+++ first-jsx/src/App.js
@@ -5,10 +5,10 @@ import NewComponent from './components/newComponent';
 function App() {
 	return (
 		<div className="App">
-			<NewComponent firstName = {"jane"} lastName = {"doe"} age = {"25"} hairColor = {"black"}/>
-			<NewComponent firstName = {"john"} lastName = {"smith"} age = {"85"} hairColor = {"brown"}/>
-			<NewComponent firstName = {"maria"} lastName = {"smith"} age = {"55"} hairColor = {"brown"}/>
-			<NewComponent firstName = {"fillmore"} lastName = {"Millard"} age = {"65"} hairColor = {"brown"}/>
+			<NewComponent firstName = {"jane"} lastName = {"doe"} age = {25} hairColor = {"black"}/>
+			<NewComponent firstName = {"john"} lastName = {"smith"} age = {85} hairColor = {"brown"}/>
+			<NewComponent firstName = {"maria"} lastName = {"smith"} age = {55} hairColor = {"brown"}/>
+			<NewComponent firstName = {"fillmore"} lastName = {"Millard"} age = {65} hairColor = {"brown"}/>
 		</div>
 	);
 }
--- first-jsx/src/components/newComponent.jsx
+++ first-jsx/src/components/newComponent.jsx
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 
 class NewComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {age : this.props.age}
+    }
+
+    setAge = () => {
+        this.setState({age : this.state.age += 1})
+        console.log(this.state.age)
+    }
+
     render() {
-        // const {firstName, lastName, age, hairColor} = this.props
         return (
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>{this.props.age}</p>
+                <p>{this.state.age}</p>
                 <p>{this.props.hairColor}</p>
+                <button onClick={this.setAge}>Birthday Button</button>
             </div>
         );
     }
